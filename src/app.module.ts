c659f13eafe53a0e975188b99cee61f7783cdb22
A.A.A <abdalrahman.webdev@gmail.com>
2024-10-04 14:58:53 +0300

    create new modules and sub modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@
 
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { MessagesModule } from './chat/messages/messages.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule, ChatModule],
   controllers: [],
   providers: [],
   exports: [],
 })
-export class AppModule {
-  
-}
+export class AppModule {}
