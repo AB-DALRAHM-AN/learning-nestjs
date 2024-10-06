c84556edf3153d4ff704301b6d7496e06fc7f873
A.A.A <abdalrahman.webdev@gmail.com>
2024-10-06 12:23:27 +0300

    Delete and Put methods

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 interface Product {
@@ -23,12 +23,14 @@ export class ProductsController {
     },
   ];
   
-  // Controller logic
+
+  // Get method to get all products
   @Get()
   getProducts() {
     return this.products;
   }
 
+  // Get method to get a product by id
   @Get(':id') // * /products/1
   getProductById(@Param('id') id: string) {
     if (!id || !this.products.find((product) => product.id === +id)) {
@@ -37,6 +39,7 @@ export class ProductsController {
     return this.products.find((product) => product.id === +id);
   }
 
+  // Post method to add a product
   @Post('') // * /products
   addProduct(@Req() request: Request) {
     const newProduct = {
@@ -45,5 +48,27 @@ export class ProductsController {
     }
     this.products.push(newProduct);
     return newProduct;
+  }
+
+  // Delete method to delete a product
+  @Delete(':id') // * /products/1
+  deleteProduct(@Param('id') id: string) {
+    const productIndex = this.products.findIndex((product) => product.id === +id);
+    if (productIndex === -1) {
+      return 'No product found';
+    }
+    this.products.splice(productIndex, 1);
+    return { message: 'Product deleted', Products: this.products };
+  } 
+
+  // Put method to update a product
+  @Put(':id') // * /products/1
+  updateProduct(@Param('id') id: string, @Body() body: Product) {
+    const productIndex = this.products.findIndex((product) => product.id === +id);
+    if (productIndex === -1) {
+      return 'No product found';
+    }
+    this.products[productIndex].title = body.title;
+    return { message: 'Product updated', product: this.products[productIndex] };
   }
 }
